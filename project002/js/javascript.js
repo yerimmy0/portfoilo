4ab8c7c6368d11c8fdbea8a85eb5a81745af2102
yerimmy0 <129369126+yerimmy0@users.noreply.github.com>
2023-06-22 16:33:40 +0900

    230622

--- project002/js/javascript.js
+++ project002/js/javascript.js
@@ -24,6 +24,29 @@ $('.box2 ul').stop().animate({'left':-wd*i},800)
         
 })
 
+setTimeout(function(){
+                        $('.hero_txt').fadeIn();
+                        },700)
+setTimeout(function(){
+                        $('.box1-1').fadeIn();
+                        },1600)
+setTimeout(function(){
+                        $('.box1-3').fadeIn();
+                        },2400)
+setTimeout(function(){
+                        $('.box1-5').fadeIn();
+                        },3200)
+setTimeout(function(){
+                        $('.box1-2').fadeIn();
+                        },4000)
+setTimeout(function(){
+                        $('.box1-4').fadeIn();
+                        },4800)
 
+    /*img2를 클릭했을 때 img1을 보여줌*/
+    $('.heartBox').click(function(){
+        $('.heartBox img').eq(0).css({'opacity': 0})
+        $('.heartBox img').eq(1).css({'opacity': 1})
+    });
 
 })
